3845f5d952c64bd359f9d70d7005f1cb4f40cad3
ariayang <ariayang@hotmail.com>
2021-07-12 12:50:14 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Objectives for this section</h1>
+    <p> The subjects to learn </p>
+    <ul>
+      <li> Learning JSX</li>
+      <li> Learning Sandbox </li>
+      <li> Learning React </li>
+    </ul>
+  </div>,
+
+  document.getElementById("root")
+);
